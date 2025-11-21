2b2265e1291b3a787b28ffdce475c06a0486d608
W Santos <51366379+ahzzano@users.noreply.github.com>
2025-11-21 14:55:32 +0800

    feat: add listener to main()

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
+use tokio::net::TcpListener;
+use tokio::net::TcpStream;
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:25565").await.unwrap();
+
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        process_socket(socket).await;
+    }
 }
+
+async fn process_socket(mut socket: TcpStream) {}
