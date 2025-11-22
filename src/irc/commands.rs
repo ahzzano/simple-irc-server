9602761e5e76ccb6e3a9b38f40959f95e96db010
W Santos <51366379+ahzzano@users.noreply.github.com>
2025-11-22 10:40:42 +0800

    feat: add PASS command

--- src/irc/commands.rs
+++ src/irc/commands.rs
@@ -1,9 +1,21 @@
 #[derive(Debug)]
-enum Command {
+pub enum Command {
     PASS(String),
     NICK(String),
+    USER(String, String, String, String),
 }
 
-pub fn parse_command(message: String) -> Option<Command> {
-    None
+pub fn parse_command(message: &String) -> Option<Command> {
+    if message.starts_with(":") {
+        None
+    } else {
+        let string: Vec<&str> = message.split(" ").collect();
+        let cmd = string[0];
+
+        return match cmd {
+            "NICK" => Some(Command::NICK(String::from(string[1]))),
+            "PASS" => Some(Command::PASS(String::from(string[1]))),
+            _ => None,
+        };
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+use crate::irc::commands::parse_command;
+use tokio::io::AsyncBufReadExt;
 use tokio::io::AsyncReadExt;
+use tokio::io::AsyncWriteExt;
+use tokio::io::BufReader;
 use tokio::net::TcpListener;
 use tokio::net::TcpStream;
+use tokio::sync::mpsc;
 
 pub mod irc;
 
@@ -14,11 +19,34 @@ async fn main() {
     }
 }
 
-async fn process_socket(mut socket: TcpStream) {
-    let mut buffer = String::new();
-    let peer_address = socket.peer_addr().unwrap();
-    let _new_user = irc::User::default();
+async fn process_socket(socket: TcpStream) {
+    let (read, mut write) = tokio::io::split(socket);
+    let mut reader = BufReader::new(read);
 
-    socket.read_to_string(&mut buffer).await.unwrap();
-    println!("{peer_address:?}: {buffer}");
+    let (tx, mut rx) = mpsc::channel(128);
+
+    tokio::spawn(async move {
+        loop {
+            let mut buffer = String::new();
+
+            match reader.read_line(&mut buffer).await {
+                Ok(0) => {
+                    println!("Reader disconnected");
+                    break;
+                }
+                Ok(_) => {
+                    let _ = tx.send(buffer.trim().to_string()).await;
+                    println!("Got message");
+                }
+                Err(_) => todo!(),
+            }
+        }
+    });
+
+    while let Some(message) = rx.recv().await {
+        println!("GOT = {message}");
+        let _cmd = parse_command(&message);
+
+        let _ = write.write_all(b"walpurgisnact\n").await;
+    }
 }
