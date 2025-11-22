9f225eea74930e26ade99c280f6ba07e91b8508b
W Santos <51366379+ahzzano@users.noreply.github.com>
2025-11-22 10:35:17 +0800

    fix: remove deadlock

--- tests/test_protocols.py
+++ tests/test_protocols.py
@@ -4,18 +4,17 @@ HOST = "127.0.0.1"
 PORT = 25565  
 
 stuff_to_send = [
-	"NICK YouMakeMeNervous",
-	"PASS I_come_from_a_land_down_under"
+	"NICK YouMakeMeNervous\n",
+	"PASS I_come_from_a_land_down_under\n"
 ]
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 	s.connect((HOST, PORT))
 
 	for st in stuff_to_send:
-		print(f"sending \"{st}\"...")
+		print(f"sending \"{st.strip()}\"...")
 		s.sendall(bytes(st, 'utf-8'))
 		
 		res = s.recv(1024)
-		print(res)
 
 	print('finished')
